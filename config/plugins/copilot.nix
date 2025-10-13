1fcb5180c1e447be4a40510c4046f256823be4c5
Matheus de Souza <maths.souza98@gmail.com>
2025-10-13 12:22:55 -0300

    fix(plugins/copilot): use new version of nodejs This is required to make copilot use the newer version of node. Also add nodejs as a dependency to copilot.

--- config/plugins/copilot.nix
+++ config/plugins/copilot.nix
@@ -1,7 +1,17 @@
+{ pkgs, ... }:
+
 {
+  extraPackages = with pkgs; [
+    nodejs
+  ];
+
   plugins = {
     copilot-vim = {
       enable = true;
     };
   };
+
+  extraConfigLua = ''
+    vim.g.copilot_node_command = "node"
+  '';
 }
