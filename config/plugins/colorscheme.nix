bc9424bc43b7f2a5e99a7d86742a556fcb2dcfbc
Matheus de Souza <maths.souza98@gmail.com>
2025-10-14 16:19:48 -0300

    feat: update kanagawa colorscheme transparency

--- config/plugins/colorscheme.nix
+++ config/plugins/colorscheme.nix
@@ -25,6 +25,12 @@
       overrides = function(colors)
 	local theme = colors.theme
 	return {
+	  NormalFloat = { bg = "none" },
+	  FloatBorder = { bg = "none" },
+	  FloatTitle = { bg = "none" },
+	  NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+	  LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+	  MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 	  Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },  -- add `blend = vim.o.pumblend` to enable transparency
 	  PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
 	  PmenuSbar = { bg = theme.ui.bg_m1 },
@@ -40,5 +46,5 @@
       end,
     })
     vim.cmd("colorscheme kanagawa")
-    '';
+  '';
 }
