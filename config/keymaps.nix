662bea1f733ce860aa44977e68cd65d2b29832b2
Matheus de Souza <maths.souza98@gmail.com>
2025-11-08 04:21:45 -0300

    feat: make all LSP related keybinds work when `LspAttach` event is triggered

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,203 +1,11 @@
 {
   keymaps = [
     {
-      key = "K";
-      action = "<cmd>lua vim.lsp.buf.hover()<cr>";
-      mode = "n";
-      options = {
-        desc = "Hover Documentation";
-        silent = true;
-      };
-    }
-    {
       key = "<leader>=";
-      action = "<cmd>lua vim.lsp.buf.format({ async = true })<cr>";
+      action = "gg=G``";
       mode = "n";
       options = {
-        desc = "Format buffer";
-        silent = true;
-      };
-    }
-    {
-      key = "<leader>rn";
-      action = "<cmd>lua vim.lsp.buf.rename()<cr>";
-      mode = "n";
-      options = {
-        desc = "Rename";
-        silent = true;
-      };
-    }
-    {
-      key = "<leader>ca";
-      action = "<cmd>lua vim.lsp.buf.code_action()<cr>";
-      mode = "n";
-      options = {
-        desc = "Code Actions";
-        silent = true;
-      };
-    }
-    {
-      key = "gt";
-      action.__raw = "function() Snacks.picker.lsp_type_definitions() end";
-      mode = "n";
-      options = {
-        desc = "Go to Type Definition";
-        silent = true;
-      };
-    }
-    {
-      mode = "n";
-      key = "gd";
-      action.__raw = "function() Snacks.picker.lsp_definitions() end";
-      options.desc = "Go to Definition";
-    }
-    {
-      mode = "n";
-      key = "gD";
-      action.__raw = "function() Snacks.picker.lsp_declarations() end";
-      options.desc = "Go to Declaration";
-    }
-    {
-      mode = "n";
-      key = "<leader>rf";
-      action.__raw = "function() Snacks.picker.lsp_references() end";
-      options = {
-        desc = "References";
-        nowait = true;
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>ip";
-      action.__raw = "function() Snacks.picker.lsp_implementations() end";
-      options.desc = "Goto Implementation";
-    }
-    {
-      key = "<leader>ic";
-      action.__raw = "function() Snacks.picker.lsp_incoming_calls() end";
-      mode = "n";
-      options = {
-        desc = "Incoming Calls";
-        silent = true;
-      };
-    }
-    {
-      key = "<leader>oc";
-      action.__raw = "function() Snacks.picker.lsp_outgoing_calls() end";
-      mode = "n";
-      options = {
-        desc = "Outgoing Calls";
-        silent = true;
-      };
-    }
-    {
-      mode = "n";
-      key = "<leader>dw";
-      action.__raw = "function() Snacks.picker.diagnostics() end";
-      options.desc = "Diagnostics";
-    }
-    {
-      mode = "n";
-      key = "<leader>db";
-      action.__raw = "function() Snacks.picker.diagnostics_buffer() end";
-      options.desc = "Buffer Diagnostics";
-    }
-    {
-      key = "<leader>dl";
-      action = "<cmd>lua vim.diagnostic.open_float()<cr>";
-      mode = "n";
-      options = {
-        desc = "Line Diagnostics";
-        silent = true;
-      };
-    }
-    {
-      key = "]e";
-      action = "<cmd>lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })<cr>";
-      mode = "n";
-      options = {
-        desc = "Next Error";
-        silent = true;
-      };
-    }
-    {
-      key = "[e";
-      action = "<cmd>lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })<cr>";
-      mode = "n";
-      options = {
-        desc = "Previous Error";
-        silent = true;
-      };
-    }
-    {
-      key = "]w";
-      action = "<cmd>lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })<cr>";
-      mode = "n";
-      options = {
-        desc = "Next Warning";
-        silent = true;
-      };
-    }
-    {
-      key = "[w";
-      action = "<cmd>lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })<cr>";
-      mode = "n";
-      options = {
-        desc = "Previous Warning";
-        silent = true;
-      };
-    }
-    {
-      key = "]h";
-      action = "<cmd>lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.HINT })<cr>";
-      mode = "n";
-      options = {
-        desc = "Next Hint";
-        silent = true;
-      };
-    }
-    {
-      key = "[h";
-      action = "<cmd>lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.HINT })<cr>";
-      mode = "n";
-      options = {
-        desc = "Previous Hint";
-        silent = true;
-      };
-    }
-    {
-      key = "]i";
-      action = "<cmd>lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.INFO })<cr>";
-      mode = "n";
-      options = {
-        desc = "Next Info";
-        silent = true;
-      };
-    }
-    {
-      key = "[i";
-      action = "<cmd>lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.INFO })<cr>";
-      mode = "n";
-      options = {
-        desc = "Previous Info";
-        silent = true;
-      };
-    }
-    {
-      key = "[d";
-      action = "<cmd>lua vim.diagnostic.goto_prev()<cr>";
-      mode = "n";
-      options = {
-        desc = "Previous Diagnostic";
-        silent = true;
-      };
-    }
-    {
-      key = "]d";
-      action = "<cmd>lua vim.diagnostic.goto_next()<cr>";
-      mode = "n";
-      options = {
-        desc = "Next Diagnostic";
+        desc = "Format buffer (fallback)";
         silent = true;
       };
     }
@@ -698,6 +506,95 @@
         desc = "Amend last commit";
         silent = true;
       };
+    }
+  ];
+
+  autoGroups.lsp_keybinds = {
+    clear = true;
+  };
+
+  autoCmd = [
+    {
+      event = "LspAttach";
+      group = "lsp_keybinds";
+      callback.__raw = ''
+        function(args)
+          local bufnr = args.buf
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          
+          vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, 
+            { buffer = bufnr, desc = "Hover Documentation", silent = true })
+          
+          vim.keymap.set("n", "<leader>=", function() vim.lsp.buf.format({ async = true }) end, 
+            { buffer = bufnr, desc = "Format buffer", silent = true })
+          
+          vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, 
+            { buffer = bufnr, desc = "Rename", silent = true })
+          
+          vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, 
+            { buffer = bufnr, desc = "Code Actions", silent = true })
+          
+          vim.keymap.set("n", "gt", function() Snacks.picker.lsp_type_definitions() end, 
+            { buffer = bufnr, desc = "Go to Type Definition", silent = true })
+          
+          vim.keymap.set("n", "gd", function() Snacks.picker.lsp_definitions() end, 
+            { buffer = bufnr, desc = "Go to Definition" })
+          
+          vim.keymap.set("n", "gD", function() Snacks.picker.lsp_declarations() end, 
+            { buffer = bufnr, desc = "Go to Declaration" })
+          
+          vim.keymap.set("n", "<leader>rf", function() Snacks.picker.lsp_references() end, 
+            { buffer = bufnr, desc = "References", nowait = true })
+          
+          vim.keymap.set("n", "<leader>ip", function() Snacks.picker.lsp_implementations() end, 
+            { buffer = bufnr, desc = "Goto Implementation" })
+          
+          vim.keymap.set("n", "<leader>ic", function() Snacks.picker.lsp_incoming_calls() end, 
+            { buffer = bufnr, desc = "Incoming Calls", silent = true })
+          
+          vim.keymap.set("n", "<leader>oc", function() Snacks.picker.lsp_outgoing_calls() end, 
+            { buffer = bufnr, desc = "Outgoing Calls", silent = true })
+          
+          vim.keymap.set("n", "<leader>dw", function() Snacks.picker.diagnostics() end, 
+            { buffer = bufnr, desc = "Diagnostics" })
+          
+          vim.keymap.set("n", "<leader>db", function() Snacks.picker.diagnostics_buffer() end, 
+            { buffer = bufnr, desc = "Buffer Diagnostics" })
+          
+          vim.keymap.set("n", "<leader>dl", function() vim.diagnostic.open_float() end, 
+            { buffer = bufnr, desc = "Line Diagnostics", silent = true })
+          
+          vim.keymap.set("n", "]e", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR }) end, 
+            { buffer = bufnr, desc = "Next Error", silent = true })
+          
+          vim.keymap.set("n", "[e", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR }) end, 
+            { buffer = bufnr, desc = "Previous Error", silent = true })
+          
+          vim.keymap.set("n", "]w", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN }) end, 
+            { buffer = bufnr, desc = "Next Warning", silent = true })
+          
+          vim.keymap.set("n", "[w", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN }) end, 
+            { buffer = bufnr, desc = "Previous Warning", silent = true })
+          
+          vim.keymap.set("n", "]h", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.HINT }) end, 
+            { buffer = bufnr, desc = "Next Hint", silent = true })
+          
+          vim.keymap.set("n", "[h", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.HINT }) end, 
+            { buffer = bufnr, desc = "Previous Hint", silent = true })
+          
+          vim.keymap.set("n", "]i", function() vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.INFO }) end, 
+            { buffer = bufnr, desc = "Next Info", silent = true })
+          
+          vim.keymap.set("n", "[i", function() vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.INFO }) end, 
+            { buffer = bufnr, desc = "Previous Info", silent = true })
+          
+          vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, 
+            { buffer = bufnr, desc = "Previous Diagnostic", silent = true })
+          
+          vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, 
+            { buffer = bufnr, desc = "Next Diagnostic", silent = true })
+        end
+      '';
     }
   ];
 }
