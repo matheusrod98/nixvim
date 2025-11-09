ad4dad6dd9270e6565c224c6eb047bfd4fd3404e
Matheus de Souza <maths.souza98@gmail.com>
2025-11-09 02:51:20 -0300

    fix: trim home directory from relative path Previously, both keybinds to copy the relative path of the current file were flaky, sometimes it returned the relative path and sometimes it returned the non-relative one.

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -65,7 +65,7 @@
     }
     {
       key = "<leader>cp";
-      action = "<cmd>lua local relative_path = vim.fn.expand('%') if relative_path == \"\" then vim.notify(\"No file in current buffer\", vim.log.levels.WARN) return end vim.fn.setreg('+', relative_path) vim.notify(\"Copied to clipboard: \" .. relative_path, vim.log.levels.INFO)<cr>";
+      action = "<cmd>lua local relative_path = vim.fn.fnamemodify(vim.fn.expand('%'), ':~:.') if relative_path == \"\" then vim.notify(\"No file in current buffer\", vim.log.levels.WARN) return end vim.fn.setreg('+', relative_path) vim.notify(\"Copied to clipboard: \" .. relative_path, vim.log.levels.INFO)<cr>";
       mode = "n";
       options = {
         desc = "Copy relative file path to clipboard";
@@ -74,7 +74,7 @@
     }
     {
       key = "<leader>cl";
-      action = "<cmd>lua local relative_path = vim.fn.expand('%') if relative_path == \"\" then vim.notify(\"No file in current buffer\", vim.log.levels.WARN) return end local line_number = vim.fn.line(\".\") local file_with_line = relative_path .. \":\" .. line_number vim.fn.setreg('+', file_with_line) vim.notify(\"Copied to clipboard: \" .. file_with_line, vim.log.levels.INFO)<cr>";
+      action = "<cmd>lua local relative_path = vim.fn.fnamemodify(vim.fn.expand('%'), ':~:.') if relative_path == \"\" then vim.notify(\"No file in current buffer\", vim.log.levels.WARN) return end local line_number = vim.fn.line(\".\") local file_with_line = relative_path .. \":\" .. line_number vim.fn.setreg('+', file_with_line) vim.notify(\"Copied to clipboard: \" .. file_with_line, vim.log.levels.INFO)<cr>";
       mode = "n";
       options = {
         desc = "Copy relative file path with line number to clipboard";
