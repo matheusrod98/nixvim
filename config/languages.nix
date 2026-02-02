ee6e333342b2e2e51348e8551b3884ebe475d170
Matheus de Souza <maths.souza98@gmail.com>
2026-02-02 11:23:09 -0300

    feat: add go-enum package using buildGoModule
    Define go-enum (github.com/abice/go-enum) inline since it's not available
    in nixpkgs. Uses buildGoModule with pinned hashes for reproducibility.

---
+++ config/languages.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+
+let
+  go-enum = pkgs.buildGoModule rec {
+    pname = "go-enum";
+    version = "0.6.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "abice";
+      repo = "go-enum";
+      rev = "v${version}";
+      hash = "sha256-Mt45Qz8l++bvBLKEpbX0m8iTkHDpsZtdYhhHUprQKY8=";
+    };
+
+    vendorHash = "sha256-YzIVI+PLZt24s/KjTxifWrvjrIU8jLvkC1lgw4yG6cg=";
+  };
+in
+{
+  extraPackages = with pkgs; [
+    # Golang
+    golines
+    go-tools
+    gomodifytags
+    gotest
+    iferr
+    impl
+    reftools
+    ginkgo
+    gotestsum
+    govulncheck
+    go-enum
+  ];
+}
