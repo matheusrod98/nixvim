d64b928034518c0c43ddb127af52b97c158cde42
Matheus de Souza <maths.souza98@gmail.com>
2026-02-05 10:54:41 -0300

    fix: force disable Oil icons with set_columns
    The declarative columns = [] setting alone doesn't reliably disable
    icons. Restore the extraConfigLuaPost call to oil.set_columns({})
    that was removed during the lazy loading refactor.

--- config/plugins/oil.nix
+++ config/plugins/oil.nix
@@ -1,14 +1,23 @@
 {
-  programs.nixvim.plugins.oil = {
-    enable = true;
-    settings = {
-      delete_to_trash = true;
-      skip_confirm_for_simple_edits = true;
-      watch_for_changes = true;
-      view_options = {
-        show_hidden = true;
+  programs.nixvim = {
+    plugins.oil = {
+      enable = true;
+      settings = {
+        delete_to_trash = true;
+        skip_confirm_for_simple_edits = true;
+        watch_for_changes = true;
+        view_options = {
+          show_hidden = true;
+        };
+        columns = [ ];
       };
-      columns = [ ];
     };
+
+    extraConfigLuaPost = ''
+      local ok, oil = pcall(require,'oil')
+      if ok then
+        oil.set_columns({})
+      end
+    '';
   };
 }
