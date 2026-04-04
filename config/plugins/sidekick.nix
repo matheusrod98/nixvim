ec86a13372beda5145e1dd5a0ae83cbf7800336a
Matheus de Souza <maths.souza98@gmail.com>
2026-04-04 03:33:01 -0300

    refactor: remove unnecessary deps for sidekick

--- config/plugins/sidekick.nix
+++ config/plugins/sidekick.nix
@@ -1,10 +1,19 @@
 {
-  programs.nixvim.plugins.sidekick = {
-    enable = true;
-    autoLoad = true;
-    settings = {
-      copilot = {
-        status.level.__raw = "vim.log.levels.OFF";
+  programs.nixvim = {
+    dependencies = {
+      "claude-code".enable = false;
+      copilot.enable = false;
+      gemini.enable = false;
+      opencode.enable = false;
+    };
+
+    plugins.sidekick = {
+      enable = true;
+      autoLoad = true;
+      settings = {
+        copilot = {
+          status.level.__raw = "vim.log.levels.OFF";
+        };
       };
     };
   };
