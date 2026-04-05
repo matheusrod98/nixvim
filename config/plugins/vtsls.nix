3f6cde5ede1eecb273d67e82c9486133fa075e12
Matheus de Souza <maths.souza98@gmail.com>
2026-04-05 05:44:14 -0300

    feat: add vtsls plugin

--- config/plugins/vtsls.nix
+++ config/plugins/vtsls.nix
@@ -8,254 +8,282 @@
       require("lspconfig.configs").vtsls = require("vtsls").lspconfig
     '';
 
-    userCommands = {
-      TSRestartServer = {
-        desc = "Restart TypeScript server";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.restart_tsserver(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
-          end
-        '';
-      };
+    extraConfigLua = ''
+      local vtsls_group = vim.api.nvim_create_augroup("nixvim-vtsls", { clear = true })
+      local ts_commands = {
+        "TSRestartServer",
+        "TSOpenServerLog",
+        "TSReloadProjects",
+        "TSSelectVersion",
+        "TSProjectConfig",
+        "TSSourceDefinition",
+        "TSFileReferences",
+        "TSRenameFile",
+        "TSOrganizeImports",
+        "TSSortImports",
+        "TSRemoveUnusedImports",
+        "TSFixAll",
+        "TSRemoveUnused",
+        "TSAddMissingImports",
+        "TSSourceActions",
+        "TSRenamePath",
+      }
 
-      TSOpenServerLog = {
-        desc = "Open TypeScript server log";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.open_tsserver_log(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vtsls_group,
+        callback = function(args)
+          if not args.data or not args.data.client_id then
+            return
           end
-        '';
-      };
 
-      TSReloadProjects = {
-        desc = "Reload TypeScript projects";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.reload_projects(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          local vtsls_name = require("vtsls.config").get().name
+          if not client or client.name ~= vtsls_name then
+            return
           end
-        '';
-      };
 
-      TSSelectVersion = {
-        desc = "Select TypeScript version";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.select_ts_version(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+          local bufnr = args.buf
+
+          vim.schedule(function()
+            pcall(vim.api.nvim_buf_del_user_command, bufnr, "VtsExec")
+            pcall(vim.api.nvim_del_user_command, "VtsRename")
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSRestartServer", function()
+              require("vtsls").commands.restart_tsserver(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Restart TypeScript server",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSOpenServerLog", function()
+              require("vtsls").commands.open_tsserver_log(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Open TypeScript server log",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSReloadProjects", function()
+              require("vtsls").commands.reload_projects(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Reload TypeScript projects",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSSelectVersion", function()
+              require("vtsls").commands.select_ts_version(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Select TypeScript version",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSProjectConfig", function()
+              require("vtsls").commands.goto_project_config(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Open tsconfig or jsconfig",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSSourceDefinition", function()
+              require("vtsls").commands.goto_source_definition(
+                vim.api.nvim_get_current_win(),
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Go to source definition",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSFileReferences", function()
+              require("vtsls").commands.file_references(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Show file references",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSRenameFile", function()
+              require("vtsls").commands.rename_file(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Rename current file and update imports",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSOrganizeImports", function()
+              require("vtsls").commands.organize_imports(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Organize imports",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSSortImports", function()
+              require("vtsls").commands.sort_imports(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Sort imports",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSRemoveUnusedImports", function()
+              require("vtsls").commands.remove_unused_imports(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Remove unused imports",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSFixAll", function()
+              require("vtsls").commands.fix_all(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Apply all TypeScript fixes",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSRemoveUnused", function()
+              require("vtsls").commands.remove_unused(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Remove unused code",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSAddMissingImports", function()
+              require("vtsls").commands.add_missing_imports(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Add missing imports",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSSourceActions", function()
+              require("vtsls").commands.source_actions(
+                bufnr,
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              desc = "Show TypeScript source actions",
+              force = true,
+            })
+
+            vim.api.nvim_buf_create_user_command(bufnr, "TSRenamePath", function(opts)
+              if #opts.fargs ~= 2 then
+                vim.notify("TSRenamePath expects exactly 2 paths", vim.log.levels.ERROR)
+                return
               end
-            )
-          end
-        '';
-      };
 
-      TSProjectConfig = {
-        desc = "Open tsconfig or jsconfig";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.goto_project_config(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
-          end
-        '';
-      };
+              require("vtsls").rename(
+                opts.fargs[1],
+                opts.fargs[2],
+                function() end,
+                function(err)
+                  vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
+                end
+              )
+            end, {
+              complete = "file",
+              desc = "Rename file or folder and update imports",
+              force = true,
+              nargs = "+",
+            })
 
-      TSSourceDefinition = {
-        desc = "Go to source definition";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.goto_source_definition(
-              vim.api.nvim_get_current_win(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
-          end
-        '';
-      };
+            vim.b[bufnr].ts_vtsls_commands = true
+          end)
+        end,
+      })
 
-      TSFileReferences = {
-        desc = "Show file references";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.file_references(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
+      vim.api.nvim_create_autocmd("LspDetach", {
+        group = vtsls_group,
+        callback = function(args)
+          if not vim.b[args.buf].ts_vtsls_commands then
+            return
           end
-        '';
-      };
 
-      TSRenameFile = {
-        desc = "Rename current file and update imports";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.rename_file(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
+          for _, command in ipairs(ts_commands) do
+            pcall(vim.api.nvim_buf_del_user_command, args.buf, command)
           end
-        '';
-      };
 
-      TSOrganizeImports = {
-        desc = "Organize imports";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.organize_imports(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
-          end
-        '';
-      };
-
-      TSSortImports = {
-        desc = "Sort imports";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.sort_imports(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
-          end
-        '';
-      };
-
-      TSRemoveUnusedImports = {
-        desc = "Remove unused imports";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.remove_unused_imports(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
-          end
-        '';
-      };
-
-      TSFixAll = {
-        desc = "Apply all TypeScript fixes";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.fix_all(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
-          end
-        '';
-      };
-
-      TSRemoveUnused = {
-        desc = "Remove unused code";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.remove_unused(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
-          end
-        '';
-      };
-
-      TSAddMissingImports = {
-        desc = "Add missing imports";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.add_missing_imports(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
-          end
-        '';
-      };
-
-      TSSourceActions = {
-        desc = "Show TypeScript source actions";
-        command.__raw = ''
-          function()
-            require("vtsls").commands.source_actions(
-              vim.api.nvim_get_current_buf(),
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
-          end
-        '';
-      };
-
-      TSRenamePath = {
-        desc = "Rename file or folder and update imports";
-        nargs = "+";
-        complete = "file";
-        command.__raw = ''
-          function(opts)
-            if #opts.fargs ~= 2 then
-              vim.notify("TSRenamePath expects exactly 2 paths", vim.log.levels.ERROR)
-              return
-            end
-
-            require("vtsls").rename(
-              opts.fargs[1],
-              opts.fargs[2],
-              function() end,
-              function(err)
-                vim.notify(type(err) == "string" and err or vim.inspect(err), vim.log.levels.ERROR)
-              end
-            )
-          end
-        '';
-      };
-    };
+          pcall(vim.api.nvim_del_user_command, "VtsRename")
+          vim.b[args.buf].ts_vtsls_commands = nil
+        end,
+      })
+    '';
   };
 }
