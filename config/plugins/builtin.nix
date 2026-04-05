203564e09b48fea69a95a13ac0e6affcd898ee9c
Matheus de Souza <maths.souza98@gmail.com>
2026-04-04 21:13:50 -0300

    feat: enable neovim 0.12 ui2 and native lsp progress
    enable ui2 to remove press enter prompt and show messages inline
    in cmdline. add lspprogress autocmd using nvim_echo with kind=progress
    for native terminal progress indicators.

--- config/plugins/builtin.nix
+++ config/plugins/builtin.nix
@@ -3,5 +3,10 @@
     vim.cmd.packadd("cfilter")
     vim.cmd.packadd("nvim.difftool")
     vim.cmd.packadd("nvim.undotree")
+
+    require("vim._core.ui2").enable({
+      enable = true,
+      msg = { targets = "cmd" },
+    })
   '';
 }
