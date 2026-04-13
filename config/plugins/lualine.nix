5231c84c981dd83271bf08e1e3a74b6058dec824
Matheus de Souza <maths.souza98@gmail.com>
2026-04-13 02:13:20 -0300

    feat: show macro recording in lualine

--- config/plugins/lualine.nix
+++ config/plugins/lualine.nix
@@ -1,4 +1,32 @@
 {
+  programs.nixvim.extraConfigLuaPre = ''
+    local macro_recording = ""
+
+    function _G.nixvim_macro_recording()
+      return macro_recording
+    end
+
+    local function refresh_lualine()
+      local ok, lualine = pcall(require, "lualine")
+      if ok then
+        lualine.refresh({ place = { "statusline" } })
+      end
+    end
+
+    vim.api.nvim_create_autocmd({ "RecordingEnter", "RecordingLeave" }, {
+      group = vim.api.nvim_create_augroup("nixvim_lualine_macro_recording", { clear = true }),
+      callback = function(ev)
+        if ev.event == "RecordingEnter" then
+          macro_recording = "Recording @" .. vim.fn.reg_recording()
+        else
+          macro_recording = ""
+        end
+
+        refresh_lualine()
+      end,
+    })
+  '';
+
   programs.nixvim.plugins.lualine = {
     enable = true;
     settings = {
@@ -10,7 +38,7 @@
         lualine_b = [ "branch" "diff" "diagnostics" ];
         lualine_c = [ "filename" ];
         lualine_x = [ "lsp_status" "encoding" "fileformat" "filetype" ];
-        lualine_y = [ "searchcount" "progress" ];
+        lualine_y = [ "nixvim_macro_recording()" "searchcount" "progress" ];
         lualine_z = [ "location" ];
       };
     };
