a1829ffb70304031b2a9e09c54833a42a37319b3
Matheus de Souza <maths.souza98@gmail.com>
2026-05-24 03:17:09 -0300

    feat(plugins/auto-session): save sessions opened with file args

--- config/plugins/auto-session.nix
+++ config/plugins/auto-session.nix
@@ -2,6 +2,7 @@
   programs.nixvim.plugins.auto-session = {
     enable = true;
     settings = {
+      args_allow_files_auto_save = true;
       auto_save = true;
       auto_restore = true;
     };
